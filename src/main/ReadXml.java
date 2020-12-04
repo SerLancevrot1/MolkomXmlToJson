0442b946bbd57b27a547c53b0d04c8cb2eaed74b
max <limon.8bit@gmail.com>
2020-12-04 21:33:29 +0300

    q

--- src/main/ReadXml.java
+++ src/main/ReadXml.java
@@ -28,90 +28,6 @@ public class ReadXml {
     }
 
     public void parseXml(){
-       File file = findFile();
-        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-
-        DocumentBuilder documentBuilder = null;
-        try {
-             documentBuilder = documentBuilderFactory.newDocumentBuilder();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
-
-        Document document = null;
-        try {
-             document = documentBuilder.parse(file);
-        } catch (SAXException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-
-        Node node1 = document.getElementsByTagName("FILE_ID").item(0);
-        System.out.printf("{\n\"FILE_ID\": \"%s\", \n" , node1.getTextContent());
-
-
-
-        //System.out.println(document.getDocumentElement().getNodeName());
-
-        NodeList nodeListHead = document.getElementsByTagName("HEADER");
-        Node node = nodeListHead.item(0);
-
-
-            if (node.getNodeType() == Node.ELEMENT_NODE) {
-
-                Element eElement = (Element) node;
-                System.out.printf("\"%s\": { \n" , node.getNodeName());
-
-                System.out.printf("\"INVOICE\": \"%s\", \n", eElement.getElementsByTagName("INVOICE").item(0).getTextContent());
-                System.out.printf("\"DATE\": \"%s\", \n" , eElement.getElementsByTagName("DATE").item(0).getTextContent());
-                System.out.printf("\"ORDER_TYPE\": \"%s\", \n" , eElement.getElementsByTagName("ORDER_TYPE").item(0).getTextContent());
-                System.out.printf("\"EST_SHIP_DATE\": \"%s\", \n" , eElement.getElementsByTagName("EST_SHIP_DATE").item(0).getTextContent());
-                System.out.printf("\"SHIPMENT_METHOD\": \"%s\", \n" , eElement.getElementsByTagName("SHIPMENT_METHOD").item(0).getTextContent());
-                System.out.printf("\"DELYVERY_SERVICE\": \"%s\", \n" , eElement.getElementsByTagName("DELYVERY_SERVICE").item(0).getTextContent());
-                System.out.printf("\"AmountwVAT\": \"%s\", \n" , eElement.getElementsByTagName("AmountwVAT").item(0).getTextContent());
-                System.out.printf("\"PaymentMethod\": \"%s\", \n" , eElement.getElementsByTagName("PaymentMethod").item(0).getTextContent());
-                System.out.printf("\"Phone\": \"%s\", \n" , eElement.getElementsByTagName("Phone").item(0).getTextContent());
-
-            }
-
-
-        NodeList nodeListDetal = document.getElementsByTagName("DETAIL");
-
-        for (int i =0; i < nodeListDetal.getLength(); i++){
-
-            // длинна -1 без запятой
-            Node nodeDetal = nodeListDetal.item(i);
-
-            //System.out.println("\nCurrent Element :" + node.getNodeName());
-
-            if (nodeDetal.getNodeType() == Node.ELEMENT_NODE)  {
-
-                Element eElement = (Element) nodeDetal;
-
-                System.out.println("N_STR : " + eElement.getElementsByTagName("N_STR").item(0).getTextContent());
-                System.out.println("STOCK_NUMBER : " + eElement.getElementsByTagName("STOCK_NUMBER").item(0).getTextContent());
-                System.out.println("STOCK_NAME : " + eElement.getElementsByTagName("STOCK_NAME").item(0).getTextContent());
-                System.out.println("MeasureUnit : " + eElement.getElementsByTagName("MeasureUnit").item(0).getTextContent());
-                System.out.println("UnitPrice : " + eElement.getElementsByTagName("UnitPrice").item(0).getTextContent());
-                System.out.println("UnitVAT : " + eElement.getElementsByTagName("UnitVAT").item(0).getTextContent());
-                System.out.println("PRICE : " + eElement.getElementsByTagName("PRICE").item(0).getTextContent());
-                System.out.println("LinePrice : " + eElement.getElementsByTagName("LinePrice").item(0).getTextContent());
-                System.out.println("LineVAT : " + eElement.getElementsByTagName("LineVAT").item(0).getTextContent());
-                System.out.println("SUM : " + eElement.getElementsByTagName("SUM").item(0).getTextContent());
-                System.out.println("LineDiscount : " + eElement.getElementsByTagName("LineDiscount").item(0).getTextContent());
-                System.out.println("QTYEXPECTED : " + eElement.getElementsByTagName("QTYEXPECTED").item(0).getTextContent());
-                System.out.println("LineAmount : " + eElement.getElementsByTagName("LineAmount").item(0).getTextContent());
-
-            }
-        }
-
-    }
-
-    public void anatherParser(){
-
         File file = findFile();
         DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
 
@@ -154,7 +70,7 @@ public class ReadXml {
                     // основной вывод
                     System.out.printf("\n\"%s\": \"%s\", ",   childNode.getNodeName().trim(),  childNode.getTextContent());
                 }
-                
+
             }
         }
 
@@ -175,6 +91,7 @@ public class ReadXml {
                     //внутри одной DETAIL. АААААА, как же люблю for
                     for(int n = 1; n < detailNodes.getLength() ; n++ ) {
 
+                        // detailAtribute == внетренний тег в одной DETAIL
                         Node detailAtribute = detailNodes.item(n);
 
                         if (detailNodes.item(n).getNodeType() == Node.ELEMENT_NODE){
@@ -194,6 +111,8 @@ public class ReadXml {
 
             }
         }
+
     }
+
 
 }
--- src/main/main.java
+++ src/main/main.java
@@ -4,6 +4,6 @@ public class main {
     public static void main(String[] args) {
 
         ReadXml readXml = new ReadXml();
-        readXml.anatherParser();
+        readXml.parseXml();
     }
 }
