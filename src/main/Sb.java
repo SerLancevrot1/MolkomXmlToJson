915ec518ff30cb6f531c826f364a1e11bc8fbc01
max <limon.8bit@gmail.com>
2020-12-06 15:19:40 +0300

    add FileWritter

--- src/main/Sb.java
+++ src/main/Sb.java
@@ -1,10 +1,41 @@
 package main;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Sb {
 
     public void showSb(StringBuilder stringBuilder){
-        System.out.println(" \n\n\n----------------------------------------------- \n\n\n" );
+
         System.out.println(stringBuilder);
+        String newFilePath = "C:\\Users\\max\\Desktop\\XmlToJson.txt";
+
+        try {
+            File file = new File(newFilePath);
+            if (file.createNewFile()) {
+                System.out.println("File created: " + file.getName());
+
+                try {
+                    FileWriter myWriter = new FileWriter(newFilePath);
+                    myWriter.write(String.valueOf(stringBuilder));
+                    myWriter.close();
+                    System.out.println("Successfully wrote to the file.");
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+
+            } else {
+                System.out.println("File already exists.");
+                return;
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+
 
     }
 }
